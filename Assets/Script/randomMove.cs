3a58cf0a2cdbfa59398acef330a32889cdc79c98
Kimhjung <heejung030619@gmail.com>
2025-05-20 16:24:42 +0900

    타이틀 거의 완성
    캐릭터 터치하면 멈춤 &  멘트 출력
    
    corutine 활용하면 컴퓨터 멈춤.
    
    다른 방도 도출해야할듯

--- Assets/Script/randomMove.cs
+++ Assets/Script/randomMove.cs
@@ -4,12 +4,51 @@ using UnityEngine;
 
 public class randomMove : MonoBehaviour
 {
-
+    Animator anim;
     Rigidbody2D rigid;
 
     void Start()
     {
         StartCoroutine(RandomMove());
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //마우스 클릭한 좌표값 가져오기
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //해당 좌표에 있는 오브젝트 찾기
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+            if (hit.collider != null)
+            {
+                GameObject click_obj = hit.transform.gameObject;
+                Debug.Log("click " + click_obj.name);
+                //StartCoroutine(StopMove());
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "WallL")
+        {
+            Debug.Log("충돌 Wall");
+            //이동방향으로 캐릭터 뒤집기
+            transform.localScale = new Vector2(-1, 1);
+            //벽에 닿으면 반대방향으로 이동
+            rigid.velocity = new Vector2(Random.Range(0.5f, 1.5f), 0);
+        }
+
+        if (other.collider.tag == "WallR")
+        {
+            Debug.Log("충돌 Wall");
+            transform.localScale = new Vector2(1, 1);
+            rigid.velocity = new Vector2(Random.Range(-1.5f, -0.5f), 0);
+
+        }
     }
     IEnumerator RandomMove()
     {
@@ -19,7 +58,6 @@ public class randomMove : MonoBehaviour
         {
             float dir = Random.Range(-2f, 2f);
 
-            yield return new WaitForSeconds(3);
             rigid.velocity = new Vector2(dir, 0);
 
             if (dir <= 0)
@@ -30,6 +68,21 @@ public class randomMove : MonoBehaviour
             {
                 transform.localScale = new Vector2(-1, 1);
             }
+            yield return new WaitForSeconds(3);
         }
     }
+
+    /*IEnumerator StopMove()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        while (true) 
+        {
+            rigid.velocity = new Vector2(0, 0);
+            anim.speed = 0;
+        }
+        yield return new WaitForSeconds(0f);
+        
+    }*/
 }
