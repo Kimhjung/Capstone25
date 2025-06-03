9bcaab6c2a1312f56805a9071ad423fdafcaaa1f
김희정 <heejung030619@gmail.com>
2025-06-04 00:50:00 +0900

    title 완성
    배틀 씬 제작해야 함

--- Assets/Script/randomMove.cs
+++ Assets/Script/randomMove.cs
@@ -11,6 +11,24 @@ public class randomMove : MonoBehaviour
     {
         StartCoroutine(RandomMove());
     }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "WallLeft")
+        {
+            Debug.Log("Collision" + other.gameObject.name + "left");
+            transform.localScale = new Vector2(-1, 1);
+            rigid.velocity = new Vector2(1.5f, 0);
+        }
+        else if (other.collider.tag == "WallRight")
+        {
+            Debug.Log("Collision"+ other.gameObject.name + "right");
+            transform.localScale = new Vector2(1, 1);
+            rigid.velocity = new Vector2(-1.5f, 0);
+        }
+    }
+
     IEnumerator RandomMove()
     {
         rigid=GetComponent<Rigidbody2D>();
@@ -26,10 +44,11 @@ public class randomMove : MonoBehaviour
             {
                 transform.localScale = new Vector2(1, 1);
             }
-            else if(dir >= 0)
+            else if (dir >= 0)
             {
                 transform.localScale = new Vector2(-1, 1);
             }
         }
+        
     }
 }
